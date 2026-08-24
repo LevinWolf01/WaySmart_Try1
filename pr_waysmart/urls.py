8283ade2be36dbae827c98057a9f4ce0f16ee6ab
LevinWolf01 <danie.a.lopez2020@gmail.com>
2026-08-24 09:28:49 -0500

    Se añade la base para empezar a trabajaer el proyecto. Se incluyen los archivos .gitignore, manage.py y la carpeta 'pr_waysmart'

---
+++ pr_waysmart/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import path, include
+from .import views
+from pr_waysmart import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('waysmart/', views.fn_inicio)
+]
