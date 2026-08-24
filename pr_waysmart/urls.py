3c444e78705b38bee5d71a17473aa2343ec30c14
LevinWolf01 <danie.a.lopez2020@gmail.com>
2026-08-24 12:53:54 -0500

    Se añade la 'app_destinos_servicios' y algunas configuraciones adicionales

--- pr_waysmart/urls.py
+++ pr_waysmart/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import path, include
-from .import views
 from pr_waysmart import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('waysmart/', views.fn_inicio)
+    path('waysmart/', views.fn_inicio),
+    # Las rutas del app quedan disponibles bajo /waysmart/.
+    path('waysmart/', include('app_destinos_servicios.urls')),
 ]
