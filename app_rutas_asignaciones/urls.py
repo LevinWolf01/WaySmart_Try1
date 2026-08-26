7bc7a87b6ed196b21c6fc6c5096f54b8df2702a8
arlex <anguchoolmer@gmail.com>
2026-08-26 16:23:58 -0500

    Terminar trabajo de rutas y asignaciones

--- app_rutas_asignaciones/urls.py
+++ app_rutas_asignaciones/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('rutas/', views.rutas, name='rutas'),
-    path('asignaciones/', views.asignaciones, name='asignaciones'),
+    path('rutas/', views.rutas_view, name='rutas'),
+    path('asignaciones/', views.asignaciones_view, name='asignaciones'),
 ]
