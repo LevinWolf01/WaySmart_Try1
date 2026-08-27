678c55632a7e034b28741ffdebf4f7ff762afad3
arlex <anguchoolmer@gmail.com>
2026-08-26 19:28:20 -0500

    actualizacion de las urls.py

--- pr_waysmart/urls.py
+++ pr_waysmart/urls.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import path, include
-from .import views
-from pr_waysmart import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('waysmart/', views.fn_inicio),
-    path('', include('app_rutas_asignaciones.urls')),
+    path('waysmart/', include('app_rutas_asignaciones.urls')),  # <-- Prefijo principal agregado
 ]
