bf183b7c6197bd79dcfdcf0f5d0e6d2e746cd59d
yibertpino@gmail.com <yibertpino@gmail.com>
2026-08-28 09:16:26 -0500

    feat: actualizacion de urls y vistas en app_vehiculos_colores

--- app_vehiculos_colores/urls.py
+++ app_vehiculos_colores/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('vehiculo/', views.fn_vehiculo, name='vehiculo'),  # o como se llame tu función en views.py
+    path('colores/', views.fn_colores, name='colores'),
 ]
+
--- app_vehiculos_colores/views.py
+++ app_vehiculos_colores/views.py
@@ -1,4 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def inicio(request):
-    return HttpResponse("¡Bienvenido a la sección de Vehículos y Colores!")
+    return render(request, 'app_vehiculos_colores/inicio.html') # O la plantilla que uses para inicio
+
+def fn_vehiculo(request):
+    return render(request, 'app_vehiculos_colores/vehiculo.html')
+
+def fn_colores(request):
+    return render(request, 'app_vehiculos_colores/colores.html')
