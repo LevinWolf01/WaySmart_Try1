f6a8508d3cbce702272b478900038d6c15a03a50
LevinWolf01 <danie.a.lopez2020@gmail.com>
2026-08-28 09:09:31 -0500

    bugfix: Se resuelven problemas de eliminacion de datos

--- pr_waysmart/settings.py
+++ pr_waysmart/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'pr_WaySmart.urls'
+ROOT_URLCONF = 'pr_waysmart.urls'
 
 TEMPLATES = [
     {
--- pr_waysmart/wsgi.py
+++ pr_waysmart/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pr_waysmart project.
+WSGI config for pr_WaySmart project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pr_waysmart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pr_WaySmart.settings')
 
 application = get_wsgi_application()
